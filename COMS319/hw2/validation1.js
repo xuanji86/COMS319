88e2940e366acc04370c8220304741985b253d28
xuanji86 <39474125+xuanji86@users.noreply.github.com>
2021-09-24 22:20:03 -0500

    第一次提交

---
+++ COMS319/hw2/validation1.js
@@ -0,0 +1,58 @@
+function vaildate1(){
+    valCheck = true;
+    var resultNameCheck1 = nameCheck(document.forms["User information"]["FirstName"].value);
+    var image1 = getImage(Boolean(resultNameCheck), "FirstName");
+    var labelNotifyEmail1=nameGetNotification(Boolean(resultNameCheck1), "FirstName");
+    document.getElementById("FirstName").appendChild(image1);
+    document.getElementById("FirstName").appendChild(labelNotifyEmail1);
+    var resultNameCheck2 = nameCheck(document.forms["User information"]["LastName"].value);
+    var image2 = getImage(Boolean(resultNameCheck), "LastName");
+    var labelNotifyEmail2=nameGetNotification(Boolean(resultNameCheck2), "LastName");
+    document.getElementById("LastName").appendChild(image2);
+    document.getAnimations("LastName").appendChild(labelNotifyEmail2);
+
+}
+
+function nameGetNotification(bool, ID){
+    var label = document.getElementById("labelNotify" + ID);
+    if (label == null) {
+        label = document.createElement("LABEL");
+        label.id = "labelNotify" + ID;
+        // label.className = "errorMessage";
+        label.setAttribute( 'class', 'errorMessage' );
+      }
+
+    label.innerHTML = bool ? "" : "Must contain onlyalphabetic or numericcharacters.";
+    return label;
+}
+
+function getImage(bool, ID){
+    var image = document.getElementById("image" + ID);
+    if (image == null) {
+        image = new Image(15, 15);
+        image.id = "image" + ID;
+    }
+    image.src = bool ? './correct.png' : './wrong.png';
+    return image;
+}
+function nameCheck(name) {
+
+    if (alphaNumCheck(name) == true){
+        return true;
+    }
+
+    valCheck = false;
+    return false;
+}
+
+function alphaNumCheck(entry){
+    var regName = /^[a-z0-9]+$/i;
+    if(entry != null && entry.match(regName)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
